0277e869d6464ccfb655b1edfdeaaadf66f7e640
Kuangyuan <chky@umich.edu>
2016-02-25 23:07:38 -0500

    lab1 stack

--- lab/kern/monitor.c
+++ lab/kern/monitor.c
@@ -59,7 +59,23 @@ int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
 	// Your code here.
-	return 0;
+  // TODO: chky
+  uint32_t ebp, eip, args[5];
+  uint32_t i;
+
+  ebp = read_ebp();
+	cprintf("Stack backtrace:\n");
+  while(ebp) {
+    eip = *((uint32_t *)ebp+1);
+    args[0] = *((uint32_t *)(ebp+4*2));
+    args[1] = *((uint32_t *)(ebp+4*3));
+    args[2] = *((uint32_t *)(ebp+4*4));
+    args[3] = *((uint32_t *)(ebp+4*5));
+    args[4] = *((uint32_t *)(ebp+4*6));
+    cprintf("  ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n", ebp, eip, args[0], args[1], args[2], args[3], args[4]);
+    ebp = *((uint32_t *)ebp);
+  }
+  return 0;
 }
 
 
@@ -69,58 +85,58 @@ mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 #define WHITESPACE "\t\r\n "
 #define MAXARGS 16
 
-static int
+  static int
 runcmd(char *buf, struct Trapframe *tf)
 {
-	int argc;
-	char *argv[MAXARGS];
-	int i;
+  int argc;
+  char *argv[MAXARGS];
+  int i;
 
-	// Parse the command buffer into whitespace-separated arguments
-	argc = 0;
-	argv[argc] = 0;
-	while (1) {
-		// gobble whitespace
-		while (*buf && strchr(WHITESPACE, *buf))
-			*buf++ = 0;
-		if (*buf == 0)
-			break;
+  // Parse the command buffer into whitespace-separated arguments
+  argc = 0;
+  argv[argc] = 0;
+  while (1) {
+    // gobble whitespace
+    while (*buf && strchr(WHITESPACE, *buf))
+      *buf++ = 0;
+    if (*buf == 0)
+      break;
 
-		// save and scan past next arg
-		if (argc == MAXARGS-1) {
-			cprintf("Too many arguments (max %d)\n", MAXARGS);
-			return 0;
-		}
-		argv[argc++] = buf;
-		while (*buf && !strchr(WHITESPACE, *buf))
-			buf++;
-	}
-	argv[argc] = 0;
+    // save and scan past next arg
+    if (argc == MAXARGS-1) {
+      cprintf("Too many arguments (max %d)\n", MAXARGS);
+      return 0;
+    }
+    argv[argc++] = buf;
+    while (*buf && !strchr(WHITESPACE, *buf))
+      buf++;
+  }
+  argv[argc] = 0;
 
-	// Lookup and invoke the command
-	if (argc == 0)
-		return 0;
-	for (i = 0; i < NCOMMANDS; i++) {
-		if (strcmp(argv[0], commands[i].name) == 0)
-			return commands[i].func(argc, argv, tf);
-	}
-	cprintf("Unknown command '%s'\n", argv[0]);
-	return 0;
+  // Lookup and invoke the command
+  if (argc == 0)
+    return 0;
+  for (i = 0; i < NCOMMANDS; i++) {
+    if (strcmp(argv[0], commands[i].name) == 0)
+      return commands[i].func(argc, argv, tf);
+  }
+  cprintf("Unknown command '%s'\n", argv[0]);
+  return 0;
 }
 
-void
+  void
 monitor(struct Trapframe *tf)
 {
-	char *buf;
+  char *buf;
 
-	cprintf("Welcome to the JOS kernel monitor!\n");
-	cprintf("Type 'help' for a list of commands.\n");
+  cprintf("Welcome to the JOS kernel monitor!\n");
+  cprintf("Type 'help' for a list of commands.\n");
 
 
-	while (1) {
-		buf = readline("K> ");
-		if (buf != NULL)
-			if (runcmd(buf, tf) < 0)
-				break;
-	}
+  while (1) {
+    buf = readline("K> ");
+    if (buf != NULL)
+      if (runcmd(buf, tf) < 0)
+        break;
+  }
 }
