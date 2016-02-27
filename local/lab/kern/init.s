a417762483e379c2479d0e0f95bc16a2ed68e8c0
Kuangyuan <chky@umich.edu>
2016-02-27 15:48:50 -0500

    new org

---
+++ local/lab/kern/init.s
@@ -0,0 +1,25 @@
+	.file	"init.c"
+	.stabs	"init.c",100,0,2,.Ltext0
+	.text
+.Ltext0:
+	.stabs	"gcc2_compiled.",60,0,0,0
+	.stabs	"int:t(0,1)=r(0,1);-2147483648;2147483647;",128,0,0,0
+	.stabs	"char:t(0,2)=r(0,2);0;127;",128,0,0,0
+	.stabs	"long int:t(0,3)=r(0,3);-0;4294967295;",128,0,0,0
+	.stabs	"unsigned int:t(0,4)=r(0,4);0;4294967295;",128,0,0,0
+	.stabs	"long unsigned int:t(0,5)=r(0,5);0;-1;",128,0,0,0
+	.stabs	"__int128:t(0,6)=r(0,6);0;-1;",128,0,0,0
+	.stabs	"__int128 unsigned:t(0,7)=r(0,7);0;-1;",128,0,0,0
+	.stabs	"long long int:t(0,8)=r(0,8);-0;4294967295;",128,0,0,0
+	.stabs	"long long unsigned int:t(0,9)=r(0,9);0;-1;",128,0,0,0
+	.stabs	"short int:t(0,10)=r(0,10);-32768;32767;",128,0,0,0
+	.stabs	"short unsigned int:t(0,11)=r(0,11);0;65535;",128,0,0,0
+	.stabs	"signed char:t(0,12)=r(0,12);-128;127;",128,0,0,0
+	.stabs	"unsigned char:t(0,13)=r(0,13);0;255;",128,0,0,0
+	.stabs	"float:t(0,14)=r(0,1);4;0;",128,0,0,0
+	.stabs	"double:t(0,15)=r(0,1);8;0;",128,0,0,0
+	.stabs	"long double:t(0,16)=r(0,1);16;0;",128,0,0,0
+	.stabs	"_Decimal32:t(0,17)=r(0,1);4;0;",128,0,0,0
+	.stabs	"_Decimal64:t(0,18)=r(0,1);8;0;",128,0,0,0
+	.stabs	"_Decimal128:t(0,19)=r(0,1);16;0;",128,0,0,0
+	.stabs	"void:t(0,20)=(0,20)",128,0,0,0
