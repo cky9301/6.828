f1e5a57a19b572d5e44c780907b3ddc540358078
Kuangyuan <chen930102@gmail.com>
2016-07-30 23:46:58 -0700

    lab5 ex1

--- lab/kern/env.c
+++ lab/kern/env.c
@@ -430,6 +430,10 @@ env_create(uint8_t *binary, enum EnvType type)
 
 	// If this is the file server (type == ENV_TYPE_FS) give it I/O privileges.
 	// LAB 5: Your code here.
+	// TODO: chky
+	if (type == ENV_TYPE_FS) {
+		env->env_tf.tf_eflags = (env->env_tf.tf_eflags & ~FL_IOPL_MASK) | FL_IOPL_3;
+	}
 }
 
 //
